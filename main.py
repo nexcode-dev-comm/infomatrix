9f4b01491c87f1d18b83979e60b4a63e7b234847
Dovlet <ddowlet502@gmail.com>
2026-05-15 20:08:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # На хакатоне разрешаем доступ отовсюду
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
